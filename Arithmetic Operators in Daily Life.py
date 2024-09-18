4c93fecef219d8ac53f6610e96611f7f6c4552dc
isaacdas <isaacdas10@gmail.com>
2024-09-18 05:45:54 -0400

    Assignment Additions

---
+++ Arithmetic Operators in Daily Life.py
@@ -0,0 +1,14 @@
+#Task 1
+
+bread, peanut_butter, jelly = 2.50, 3, 299999 #grape and strawberry shortage o.o
+print("The total for bread, peanut butter and jelly is...", "$",bread + peanut_butter + jelly)
+
+
+#Task 2
+
+#savings account
+initial_amount = 15000
+interest_rate = .05 #convert percentage
+final_amount = initial_amount + initial_amount * interest_rate # calculate percent add on formula
+print("$",initial_amount, "at", interest_rate * 100,"%", "interest per year yields", "$",final_amount)
+#retuen interest rate back at percent value and print statement 
--- Showcase Your Dance Moves.py
+++ Showcase Your Dance Moves.py
@@ -2,9 +2,9 @@
 
 weather = "sunny"
 if weather == "sunny":
-    print("Wear Sunglasses")
+    print("Wear Sunglasses") #wrong indentation
 else:
-    print("Take an umbrella")
+    print("Take an umbrella") #wrong indentation
 
 
 #Task 2
@@ -12,16 +12,16 @@ else:
 user_feel = "Are you feeling happy or sad"
 print(user_feel)
 user_input = input()
-if "app" in user_input:
+if "app" in user_input: #allows for capital
     print("That's great to hear!")
-elif "ad" in user_input:
+elif "ad" in user_input: #(sad, mad, bad)
     print("I hope your day gets better")
 else:
-    print("Please narrow it down to either 'happy' or 'sad' please")
-    user_input = input()
+    print("Please narrow it down to either 'happy' or 'sad' please") #reasks user if foreign input
+    user_input = input() 
     if "app" in user_input:
         print("That's great to hear!")
     elif "ad" in user_input:
         print("I hope your day gets better")
     else:
-        print("hmm, I don't seem to understand. end of program")
+        print("hmm, I don't seem to understand. end of program") #termination message
